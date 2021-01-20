d0328a191c937879bb5a6482f6193a9be31c4a34
Arthur <arthurdelbeke@smartness.co>
2021-01-20 17:52:19 +0100

    Proof of concept with react-native-collapsible

--- App.tsx
+++ App.tsx
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableWithoutFeedback, View} from 'react-native';
+import Collapsible from 'react-native-collapsible';
+import {AntDesign} from '@expo/vector-icons';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [isCollapsedDesc, setIsCollapsedDesc] = useState(true)
+    const [isCollapsedAddress, setIsCollapsedAddress] = useState(true)
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.divider} />
+            <TouchableWithoutFeedback onPress={() => setIsCollapsedDesc(prevState => !prevState)}>
+                <View style={styles.row}>
+                    <AntDesign name={isCollapsedDesc ? 'plus' : 'minus'} />
+                    <View style={{ flex: 1 }}>
+                        <Text style={{ textAlign: 'center', fontSize: 22 }}>Add description</Text>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+            <Collapsible collapsed={isCollapsedDesc}>
+                <View style={{ padding: 16 }}>
+                    <Text>The user will add a description... !</Text>
+                </View>
+            </Collapsible>
+            <View style={styles.divider} />
+            <TouchableWithoutFeedback onPress={() => setIsCollapsedAddress(prevState => !prevState)}>
+                <View style={styles.row}>
+                    <AntDesign name={isCollapsedAddress ? 'plus' : 'minus'} />
+                    <View style={{ flex: 1 }}>
+                        <Text style={{ textAlign: 'center', fontSize: 22 }}>Add address</Text>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+            <Collapsible collapsed={isCollapsedAddress}>
+                <View style={{ padding: 16 }}>
+                    <Text>The user will add an address... !</Text>
+                </View>
+            </Collapsible>
+            <View style={styles.divider} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    row: {
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        alignItems: 'center',
+        padding: 16,
+    },
+    divider: {
+        height: 5,
+        width: '100%',
+        backgroundColor: '#EDF1F7'
+    }
 });
